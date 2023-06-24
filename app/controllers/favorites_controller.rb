45a51583804c4d5d3337d317f44ab25ea9b264f2
diaraba <diarradiaraba9@gmail.com>
2023-06-24 00:56:47 +0000

    second commit, adjustement du controller favorites en remplaçant le rendu par  redirect_to, en utilisant uniquement ajax pour le rendu de fzçon asynchrone

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,28 @@
 class FavoritesController < ApplicationController
 
   def create
-    current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path
+    @favorite=current_user.favorites.build(blog_id: params[:blog_id])
+    @blog = Blog.find(params[:blog_id])
+    
+    respond_to do |format|
+      if @favorite.save
+        format.js { render :favorite }
+      end
+    end
   end
 
+
+
   def destroy
-    current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path
+    @favorite=current_user.favorites.find_by(id: params[:id])
+    @blog = @favorite.blog
+
+    if @favorite
+      @favorite.destroy!
+    end  
+    respond_to do |format|     
+      format.js { render :favorite }
+    end
   end
 
 end
